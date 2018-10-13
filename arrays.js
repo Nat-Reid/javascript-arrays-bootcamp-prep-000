642366ace19d3cce1be75184948fa2fc5835dcb5
Nat Reid <nathanielreid@gmail.com>
2018-10-13 00:52:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,3 +6,26 @@ function addElementToBeginningOfArray(array, element){
     array.unshift(element)
     return array
   }
+function addElementToEndOfArray(array, element){
+  return [...array, element]
+}
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.push(element)
+  return array
+}
+function accessElementInArray(array, index){
+  return array[index]
+}
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+}
+function removeElementFromBeginningOfArray(array){
+  return array.slice(1)
+}
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+}
+function removeElementFromEndOfArray(array){
+  return array.slice(-1)
+}
+
